bd38998d2fe7e4c1a5a82e01ebc9595ccdbb46e2
ereldebel <erel.debel@mail.huji.ac.il>
2022-04-16 23:49:10 +0300

    Fixed crow mouse following.

--- Assets/Scripts/Player/Crow.cs
+++ Assets/Scripts/Player/Crow.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 namespace Player
 {
-    public class Crow : MonoBehaviour
-    {
-        private void Update()
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePosition.z = 0f;
-            transform.position = Input.mousePosition;
-        }
-    }
+	public class Crow : MonoBehaviour
+	{
+		private Transform _transform;
+		private Camera _camera;
+
+		private void Awake()
+		{
+			_transform = transform;
+			_camera = Camera.main;
+		}
+
+		private void Update()
+		{
+			transform.position = _camera.ScreenToWorldPoint(Input.mousePosition);
+			var localPos = _transform.localPosition;
+			localPos.z += 2;
+			_transform.localPosition = localPos;
+		}
+	}
 }
